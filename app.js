5f76f99c93e1884983d16709d0fe5129104a09f3
coleyrockin <coleyrockin@aol.com>
2022-07-19 13:15:20 -0500

    Code structure

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
-
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
 
+console.log(profileDataArgs);
+
 const [name, github] = profileDataArgs;
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
--- src/page-template.js
+++ src/page-template.js
@@ -1,14 +1,15 @@
-const generatePage = (name, github) => {
+module.exports = (name, github) => {
     return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
+    <!DOCTYPE html>
+    <html lang="en">
+    
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Portfolio Demo</title>
     </head>
-  
+    
     <body>
       <h1>${name}</h1>
       <h2><a href="https://github.com/${github}">Github</a></h2>
@@ -16,5 +17,4 @@ const generatePage = (name, github) => {
     </html>
     `;
   };
-
-  module.exports = generatePage;
+  
