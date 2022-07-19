a482d79be744c17ac1596f694864633b0819fa18
coleyrockin <coleyrockin@aol.com>
2022-07-19 13:03:29 -0500

    Generating HTML file

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    //this ...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const generatePage = require('./src/page-template.js');
 
-    console.log('================');
+const profileDataArgs = process.argv.slice(2);
 
-    //Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-};
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
