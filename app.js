90e0c1edd12563a188d4c9b0b8dc1cd55e746e3e
coleyrockin <coleyrockin@aol.com>
2022-07-20 17:26:11 -0500

    Questions

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+// const pageHTML = generatePage(name, github);
 
-console.log(profileDataArgs);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const [name, github] = profileDataArgs;
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-console.log(name, github);
-
-const pageHTML = generatePage(name, github);
-
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+  .prompt([
+    {
+      type:'input',
+      name:'name',
+      message:'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
